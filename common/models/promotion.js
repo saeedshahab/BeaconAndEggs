009154f5dcd683fef2623993947ab9c762c073dc
Adhithya Ravindra <AdhithyaKuthethur.Ravindra@tesco.com>
2018-07-12 18:27:35 +0530

    [HTMLPage] Function to convert JSON to HTML table

--- common/models/promotion.js
+++ common/models/promotion.js
@@ -2,6 +2,11 @@
 const async = require('async');
 const app = require('../../server/server');
 const _ = require('lodash');
+// const Xpress = require('express');
+// var express = Xpress;
+
+
+
 module.exports = function(Promotion) {
   Promotion.getAllPromotions = (storeId, callback) => {
     app.models.Promotion.find({
@@ -29,24 +34,34 @@ module.exports = function(Promotion) {
       }
     ]
     }, (err, results) => {
-      let response = [];
+      let response = "<html><body>";
+      response += "<table border='1'>";
+      // let response = [];
       if (!err) {
         async.forEach(results, (element, callback) => {
           element = JSON.parse(JSON.stringify(element));
-          let obj = {
-            store: element.productLocation.store.location,
-            product: {
-              name: element.productLocation.product.name,
-              gtin: element.productLocation.product.gtin   
-            },
-            price: {
-              oldPrice: element.price.price,
-              newPrice: element.newPrice
-            }
-          };
-          response.push(obj);
+          response += "<tr>";
+          response += "<td>" + element.productLocation.store.location + "</td>";
+          response += "<td>" + element.productLocation.product.name + "</td>";
+          response += "<td>" + element.productLocation.product.gtin + "</td>";
+          response += "<td>" + element.price.price + "</td>";
+          response += "<td>" + element.newPrice + "</td>";
+          response += "</tr>";
+          // let obj = {
+          //   store: element.productLocation.store.location,
+          //   product: {
+          //     name: element.productLocation.product.name,
+          //     gtin: element.productLocation.product.gtin   
+          //   },
+          //   price: {
+          //     oldPrice: element.price.price,
+          //     newPrice: element.newPrice
+          //   }
+          // };
+          // response.push(obj);
           callback();
         }, (err) => {
+          response += "</table></body></html>"
           callback(err, response);
         });
       } else callback(err, response);
@@ -75,6 +90,7 @@ module.exports = function(Promotion) {
       http: {
         status: 200
       },
+      type: 'string',
       root: true
     }
   });
